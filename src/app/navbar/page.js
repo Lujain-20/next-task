ed4d5734adeaece9613a63ade9b859ca7c7cdca0
lujain <loginalkholy@gmail.com>
2025-05-06 10:54:16 +0300

    f

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -1,9 +1,5 @@
-import Link from 'next/link';
 import React from 'react';
 import { signOut } from '../auth';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
-// import Container from 'react-bootstrap/Container';
 function Navbar() {
   async function out() {
     'use server'
@@ -34,37 +30,12 @@ function Navbar() {
         <form action={out}>
           <button className='btn btn-primary' type='submit'>sign out</button>
           </form>
-          {/* <a href="/signup" className="hover:text-gray-300">
-            sign out
-          </a> */}
-        
+      
       </div>
       
       <div className="md:hidden bg-gray-800"> 
       </div>
     </nav>
-
-
-    // //////////////////////////////////////////////////////
-    // <>
-    // <Navbar className="bg-gray-900 text-white py-4">
-    //   <Container>
-    //     <Nav className="me-auto">
-    //       <Link href="/" className="text-xl font-semibold">
-    //         My Website</Link>
-    //         <Link href="/" className="hover:text-gray-300">
-    //         Home</Link>
-    //         <Link href="/about" className="hover:text-gray-300">
-    //         About</Link>
-    //         <Link href="/user" className="hover:text-gray-300">
-    //         Users</Link>
-    //         <Link href="/login" className="hover:text-gray-300">
-    //         Login</Link>
-    //     </Nav>
-
-    //   </Container>
-    // </Navbar>
-    // </>
   );
 }
 
